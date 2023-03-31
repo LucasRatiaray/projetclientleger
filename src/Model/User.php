3ba1ee53a46026820d3aa39f297f4b07026abf1c
LucasRatiaray <contact.ratiaray@gmail.com>
2023-03-31 04:21:06 +0200

    modif de user.php

--- src/Model/User.php
+++ src/Model/User.php
@@ -6,7 +6,7 @@ class User extends Model
 {
     public function getUsers()
     {
-        $sql = 'SELECT * FROM user';
+        $sql = 'SELECT * FROM users';
         $users = $this->executeQuery($sql);
         return $users;
     }
